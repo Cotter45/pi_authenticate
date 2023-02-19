be0a81bd06e17eea360ec1a6b3f4d01565ef4998
Sean Cotter <sd.cotter45@gmail.com>
2023-02-19 08:46:28 -0500

    switched to postgres before email setup

--- db/connect.go
+++ db/connect.go
@@ -2,11 +2,13 @@ package db
 
 import (
 	"fmt"
+	"os"
 	"auth/model"
 
-	"gorm.io/driver/sqlite"
+	// "gorm.io/driver/sqlite"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
+	// "gorm.io/gorm/logger"
 )
 
 // InitDB connect to db (sqlite)
@@ -16,12 +18,20 @@ func InitDB() {
 		fmt.Println(err)
 	}
 
-	DB, err = gorm.Open(
-		sqlite.Open("db/auth.db"),
-		&gorm.Config{
-			Logger: logger.Default.LogMode(logger.Silent), PrepareStmt: true,
-		})
+	// DB, err = gorm.Open(
+	// 	sqlite.Open("db/auth.db"),
+	// 	&gorm.Config{
+	// 		Logger: logger.Default.LogMode(logger.Silent), PrepareStmt: true,
+	// 	})
 
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	panic("failed to connect database")
+	// }
+
+	dsn := "host=" + os.Getenv("DB_HOST") + " user=" + os.Getenv("DB_USER") + " password=" + os.Getenv("DB_PASSWORD") + " dbname=" + os.Getenv("DB_NAME") + " port=" + os.Getenv("DB_PORT") + " sslmode=disable TimeZone=Asia/Shanghai"
+
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		fmt.Println(err)
 		panic("failed to connect database")
